95d9c02258a2d147518f258c7010c4a3785fd761
Andreas Vrachimi <avrachimi@hotmail.com>
2024-10-02 12:16:29 +0100

    Another minor auth change

--- backend/auth/goth.go
+++ backend/auth/goth.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -88,14 +89,14 @@ func (a *Auth) AuthCallback(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to generate tokens", http.StatusInternalServerError)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
-	return
-
-	// url := fmt.Sprintf("https://scorepad.general.avrachimi.com?access_token=%s&refresh_token=%s", response["access_token"], response["refresh_token"])
-	// w.Header().Set("Location", url)
-	// w.WriteHeader(http.StatusTemporaryRedirect)
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(response)
 	// return
+
+	url := fmt.Sprintf("com.scorepad:/(authenticated)/(tabs)/home?access_token=%s&refresh_token=%s", response["access_token"], response["refresh_token"])
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	return
 }
 
 func createUserTokens(r *http.Request, db *database.Queries, user goth.User) (map[string]string, error) {
